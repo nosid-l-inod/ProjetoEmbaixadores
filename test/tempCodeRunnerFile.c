d528783669af315500f4d9d26d1981d8b1adbae3
Donildison Afonso <donidilsonafonso496@gmail.com>
2025-01-10 08:24:34 +0000

    Friday changes

--- test/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-
-                // do {
-                //     addEmbaixador(embaixador);
-                //     saveData(embaixador, filename_embaixador);
-
-                //     printf("Adicionar mais embaixadores? (S/N) ");
-                //     scanf(" %c", &continuar);
-                // }
-                // while (continuar == 's' || continuar == 'S');
